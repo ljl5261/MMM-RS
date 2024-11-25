0add49933503fdf72569a19adeeca86760b851d9
LHN <134903272+LHN2002@users.noreply.github.com>
2024-11-25 19:35:32 +0800

    Update MRSCC2.0.py

--- Standardized_processing/MRSCC2.0.py
+++ Standardized_processing/MRSCC2.0.py
@@ -8,38 +8,38 @@ import numpy as np
 from realesrgan import RealESRGANer
 from basicsr.archs.rrdbnet_arch import RRDBNet
 
-# 初始化Real-ESRGAN
+
 upsampler = RealESRGANer(
-    scale=2,  # 将超分辨率倍数设置为2
-    model_path='chaofenbianlv_pth/RealESRGAN_x2plus.pth',  # 更新为实际的模型路径
-    model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2),  # 对应的scale设置为2
+    scale=2,  
+    model_path='chaofenbianlv_pth/RealESRGAN_x2plus.pth',  
+    model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2), 
     tile=0,
     tile_pad=10,
     pre_pad=0,
-    half=True  # 注意：如果您的环境支持CUDA，可以将half设置为True
+    half=True  
 )
 
 
 def super_resolve_image(image):
-    # 将PIL图像转换为cv2图像
+    # Convert a PIL image to a cv2 image.
     img_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-    # 使用Real-ESRGAN进行超分辨率处理
+    # Perform super-resolution processing using Real-ESRGAN.
     try:
-        output, _ = upsampler.enhance(img_cv2, outscale=2)  # 将outscale参数改为2倍
+        output, _ = upsampler.enhance(img_cv2, outscale=2)  
     except Exception as e:
         print(f"Error during super-resolution: {e}")
-        return image  # 如果出现错误，返回原图像
+        return image  
 
-    # 将cv2图像转换回PIL图像
+    # Convert a cv2 image back to a PIL image.
     output_image = Image.fromarray(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
-    # 调整图像大小到512x512
+    # Resize the image to 512x512.
     resized_output_image = output_image.resize((512, 512), Image.BICUBIC)
     return resized_output_image
 
 
 def process_images(input_folder, output_folder):
-    # 确保输出文件夹存在
+    
     os.makedirs(output_folder, exist_ok=True)
 
     for filename in tqdm(os.listdir(input_folder), desc="Processing images"):
@@ -54,7 +54,7 @@ def process_images(input_folder, output_folder):
                 print(f"Error processing {img_path}: {e}")
 
 
-# 路径
+# path
 input_folder = './dataset/MRSSC2.0'
 output_folder = './dataset/MRSSC2.0_jpg'
 
