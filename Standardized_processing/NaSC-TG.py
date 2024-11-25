fda138632adc9bb2c9a3cf4ac3bb171c902addb3
LHN <134903272+LHN2002@users.noreply.github.com>
2024-11-25 19:37:59 +0800

    Update NaSC-TG.py

--- Standardized_processing/NaSC-TG.py
+++ Standardized_processing/NaSC-TG.py
@@ -8,9 +8,9 @@ import numpy as np
 from realesrgan import RealESRGANer
 from basicsr.archs.rrdbnet_arch import RRDBNet
 
-# 初始化Real-ESRGAN
+
 upsampler = RealESRGANer(
-    scale=2,  
+    scale=4,  
     model_path='chaofenbianlv_pth/RealESRGAN_x4plus.pth', 
     model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4), 
     tile=0,
@@ -21,25 +21,24 @@ upsampler = RealESRGANer(
 
 
 def super_resolve_image(image):
-    # 将PIL图像转换为cv2图像
+   
     img_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-    # 使用Real-ESRGAN进行超分辨率处理
     try:
-        output, _ = upsampler.enhance(img_cv2, outscale=2)  
+        output, _ = upsampler.enhance(img_cv2, outscale=4)  
     except Exception as e:
         print(f"Error during super-resolution: {e}")
-        return image  # 如果出现错误，返回原图像
+        return image 
 
-    # 将cv2图像转换回PIL图像
+   
     output_image = Image.fromarray(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
-    # 调整图像大小到512x512
+    
     resized_output_image = output_image.resize((512, 512), Image.BICUBIC)
     return resized_output_image
 
 
 def process_images(input_folder, output_folder):
-    # 确保输出文件夹存在
+    
     os.makedirs(output_folder, exist_ok=True)
 
     for filename in tqdm(os.listdir(input_folder), desc="Processing images"):
@@ -54,7 +53,7 @@ def process_images(input_folder, output_folder):
                 print(f"Error processing {img_path}: {e}")
 
 
-# 路径
+
 input_folder = './dataset/NaSC-TG2'
 output_folder = './dataset/NaSC-TG2_jpg'
 
